9f927787790566bb4577d288e6c4a72c9aeaf3c3
ela_lisondra <emrlisondra@gmail.com>
2021-05-02 21:15:30 +0100

    get data.result

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function (){
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = usersRequest.json();
+    console.log(data);
+
+    const usersResults = data.result;
+    console.log(usersResults);
+}
+
+getData();
+
